fa4a1cf99cd61c4525b57b873f9b86e551a1d224
Kiyan <freetymesunkiyan@gmail.com>
2014-04-28 22:02:08 -0400

    Initial Commit

---
+++ FaceChangerTests/FaceChangerTests.m
@@ -0,0 +1,34 @@
+//
+//  FaceChangerTests.m
+//  FaceChangerTests
+//
+//  Created by Kiyan Liu on 4/28/14.
+//  Copyright (c) 2014 FreeTymeKiyan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FaceChangerTests : XCTestCase
+
+@end
+
+@implementation FaceChangerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
